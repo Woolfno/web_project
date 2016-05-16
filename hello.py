f60deb89a45da12464ec13f8d3f1223349f6809f
Box <box@box.org>
2016-05-16 22:09:45 +0500

    add wsgi

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def application(environ,start_response):
-	start_response=('200 OK',[('Content-Type','text/plain')])
-	body=environ['QUERY_STRING'].replace('&','\n')
+def application(env, start_response):
+	body=env['QUERY_STRING'].replace('&','\n')
+	start_response('200 OK', [('Content-Type', 'text/plain')])
 	return body
--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir -p public/img
-mkdir public/css
-mkdir public/js
-mkdir uploads
-mkdir etc
+mkdir -p public/css
+mkdir -p public/js
+mkdir -p uploads
+mkdir -p etc
 
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/site-enable/deafault
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/site-enable/deafault
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
