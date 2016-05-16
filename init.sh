16dfd685edc9bae3179df31ddf8666f07f5ea329
Box <box@box.org>
2016-05-16 15:29:31 +0500

    add wsgi

--- init.sh
+++ init.sh
@@ -7,3 +7,5 @@ mkdir etc
 
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/site-enable/deafault
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
