f1defc3298829e01d490504dc0cc48224284d460
Box <box@box.org>
2016-05-20 18:23:22 +0500

    paginator

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,8 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.http import Http404
 
 class Question(models.Model):
+    object=QuestionManager()
 	title=models.CharField(max_length=200)
 	text=models.TextField()
 	added_at=models.DateTimeField()
@@ -21,3 +23,21 @@ class Answer(models.Model):
     question=models.OneToOneField(Question)
     author=models.OneToOneField(User,
                                 related_name='+')
+
+def paginate(request,qs):
+    try:
+        limut=int(request.GET.get('limit',10))
+    except ValueError:
+        limit=10
+    if limit>100:
+        limit=10
+    try:
+        page=int(request.GET.get('page',1))
+    except ValueError:
+        raise Http404
+    paginator=Paginator(qs,limit)
+    try:
+        page=paginator.page(page)
+    except EmptyPage:
+        page=paginator.page(paginator.num_pages)
+    return page
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.views.decorators.http import require_GET
 
 def test(request,*args,**kwargs):
 	return HttpResponse('OK')
+
+@require_GET
+def questions_list(request):
+    request.GET.get('page')
